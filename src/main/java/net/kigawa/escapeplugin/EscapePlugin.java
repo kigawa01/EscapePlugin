bf6c05467708d821a58ae7d152070fe1cd55066d
kigawa <kigawa.8390@gmail.com>
2021-08-30 16:47:51 +0900

    add gate listener

--- src/main/java/net/kigawa/escapeplugin/EscapePlugin.java
+++ src/main/java/net/kigawa/escapeplugin/EscapePlugin.java
@@ -3,6 +3,7 @@ package net.kigawa.escapeplugin;
 import net.kigawa.escapeplugin.command.EscapeCommand;
 import net.kigawa.escapeplugin.command.GateCommand;
 import net.kigawa.escapeplugin.game.EscapeManager;
+import net.kigawa.escapeplugin.gate.GateListener;
 import net.kigawa.escapeplugin.gate.GateManager;
 import net.kigawa.escapeplugin.util.plugin.all.KigawaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -17,6 +18,7 @@ public final class EscapePlugin extends KigawaPlugin {
     public void onStart() {
         GateManager gateManager = new GateManager(this);
         EscapeManager escapeManager = new EscapeManager(this, gateManager);
+        GateListener gateListener=new GateListener(this,gateManager);
         addCommand(new EscapeCommand(this, escapeManager));
         addCommand(new GateCommand(this,gateManager));
     }
